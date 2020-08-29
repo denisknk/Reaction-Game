72fd5f837d9cb06495fe58ccf720d7c22214ac52
Denis <denisknk@gmail.com>
2020-08-30 00:24:03 +0300

    Updates

---
+++ precache-manifest.5a54cd8cf7eb4e0b49e18274f5fe73c0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4126505cd246c74f80810c526933b9d3",
+    "url": "/Reaction-Game/index.html"
+  },
+  {
+    "revision": "2549b63ad8364de81dc7",
+    "url": "/Reaction-Game/static/css/main.a7febd69.chunk.css"
+  },
+  {
+    "revision": "fb9be95d33e89d6fdfc1",
+    "url": "/Reaction-Game/static/js/2.3b1cfe65.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Reaction-Game/static/js/2.3b1cfe65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2549b63ad8364de81dc7",
+    "url": "/Reaction-Game/static/js/main.5f2674ba.chunk.js"
+  },
+  {
+    "revision": "c5c6a2766fba018fa94a",
+    "url": "/Reaction-Game/static/js/runtime-main.8cf85140.js"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/Reaction-Game/static/media/Roboto-Light.88823c20.ttf"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/Reaction-Game/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "321de678e592d0b8f44f1a82d7ca4b62",
+    "url": "/Reaction-Game/static/media/Roboto-Thin.321de678.ttf"
+  }
+]);
