08521e282b05a7e56985c3a5ea488df80eb3e3ec
Denis Kondratiuk <denys@Deniss-MacBook-Air.local>
2023-07-08 16:40:39 +0100

    Updates

---
+++ precache-manifest.412d4db95b9770abd99ab3a0fb6fcebc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "581eade264088cf300b22e5ead291a9f",
+    "url": "/Reaction-Game/index.html"
+  },
+  {
+    "revision": "09d8318a97e78a0306ef",
+    "url": "/Reaction-Game/static/css/main.b8ad6828.chunk.css"
+  },
+  {
+    "revision": "2d4abedd43ad81dd2a76",
+    "url": "/Reaction-Game/static/js/2.97859027.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Reaction-Game/static/js/2.97859027.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09d8318a97e78a0306ef",
+    "url": "/Reaction-Game/static/js/main.147b0dfa.chunk.js"
+  },
+  {
+    "revision": "9f21117c826cb6f9fc13",
+    "url": "/Reaction-Game/static/js/runtime-main.96578002.js"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/Reaction-Game/static/media/Roboto-Light.88823c20.ttf"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/Reaction-Game/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "321de678e592d0b8f44f1a82d7ca4b62",
+    "url": "/Reaction-Game/static/media/Roboto-Thin.321de678.ttf"
+  }
+]);
